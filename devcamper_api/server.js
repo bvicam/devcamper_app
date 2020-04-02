7bb769d268d02fcc8fad9219418c5399ea01e141
Rakesh Sherwal <Rakesh.Sherwal@XEBIAINDIA.LOCAL>
2020-04-02 20:23:47 +0530

    Create dummy api.

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -5,8 +5,39 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({
+    sucess: true,
+    msg: "Show all bootcamps",
+  });
+});
+
+// Get by ID
+app.get('/api/v1/bootcamp/:id', (req, res) => {
+  res.status(200).json({
+    sucess: true,
+    msg: `Get bootcamp ${req.params.id}`,
+  });
+});
+
+// Post call: create bootcamp
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({
+    sucess: true,
+    msg: `Create new bootcamp`,
+  });
+});
+
+//Update a bootcampby id
+app.put('/api/v1/bootcamp/:id', (req, res) => {
+  res.status(200).json({
+    sucess: true,
+    msg: `Update a bootcamp ${req.params.id}`,
+  });
+});
+
 const PORT = process.env.PORT || 5000;
-console.log('rakesh', process.env.PORT);
+
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
