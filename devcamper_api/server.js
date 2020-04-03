0bc9ed151e40dfbdd0c3f7e0b08cdfbd943f32bb
Rakesh Sherwal <Rakesh.Sherwal@XEBIAINDIA.LOCAL>
2020-04-03 16:03:33 +0530

    Connect to mongodb and add error handling.

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -6,15 +6,20 @@ const morgan = require('morgan');
 // Route files
 const bootcamps = require('./routes/bootcamps');
 const logger = require('./middleware/logger');
+const connectDB = require('./config/db')
+
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 // Bootcamps version 1
 const bootcampsApiUrl = '/api/v1/bootcamps';
 
+// Connect to DB
+connectDB();
+
+// Set console log colors
 colors.setTheme({
   silly: 'rainbow',
   input: 'grey',
@@ -36,7 +41,14 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(bootcampsApiUrl, bootcamps);
 
-app.listen(
+// Handle unhandle promise Rejection
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
+
+process.on('unhandledRejection', (err, Promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  //  Close Server
+  server.close(() => process.exit());
+});
