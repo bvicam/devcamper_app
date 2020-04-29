3bbd29e66bcf29b3e5d84ad76a0d1420857b0483
sherwalrakesh <rakesh.sherwal20@gmail.com>
2020-04-29 12:49:36 +0530

    Sign  Get JSON Web Token.

--- devcamper_api/controllers/auth.js
+++ devcamper_api/controllers/auth.js
@@ -7,13 +7,20 @@ const User = require('../models/User');
 // @access    Public
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
+
+  // Create User
   const user = await User.create({
     name,
     email,
     password,
     role
   });
+
+  // Create Token
+  const token = user.getSignedJWTToken();
+
   res.status(200).json({
-    success: true
+    success: true,
+    token
   });
 });   
